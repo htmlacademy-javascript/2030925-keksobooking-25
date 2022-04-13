7e971f147b75b5a4d60a0bf33907425017ff7e5a
Georgij Sosedov <georgijSosedov@scioskola.cz>
2022-04-13 17:36:04 +0300

    homework

--- js/main.js
+++ js/main.js
@@ -1,4 +1,3 @@
-
 function getRandom (min, max) {
   if (min >= 0 && max >= min) {
     return Math.floor(Math.random() * (max - min)) + min;
@@ -17,75 +16,79 @@ function getRandomFloat (min,max,commas = 3) {
   }
 }
 
-const type = {
-  'palace' ,
-  'flat' ,
-  'house' ,
-  'bungalow' ,
-   'hotel'
-}
+const anotherOffers = 10;
 
-const checkin = [
+const TYPE = [
+  'palace',
+  'flat',
+  'house',
+  'bungalow',
+  'hotel',
+];
+
+const CHECKIN = [
   '12:00',
   '13:00',
-  '14:00'
-]
+  '14:00',
+];
 
-const features = {
+const FEATURES = [
   'wifi',
   'dishwasher',
   'parking',
   'washer',
   'elevator',
-  'conditioner'
-}
+  'conditioner',
+];
 
-const photos = {
+const PHOTOS = [
   'https://assets.htmlacademy.ru/content/intensive/javascript-1/keksobooking/duonguyen-8LrGtIxxa4w.jpg' ,
   'https://assets.htmlacademy.ru/content/intensive/javascript-1/keksobooking/brandon-hoogenboom-SNxQGWxZQi0.jpg' ,
-  'https://assets.htmlacademy.ru/content/intensive/javascript-1/keksobooking/claire-rendall-b6kAwr1i0Iw.jpg'
-}
-
-const author = {
-  avatar: 'img/avatars/user' + xx + '.png'
-};
-let xx = '';
-let authorArray = [];
-let number = 1;
-
-for (let i = 0 ; i >= 10 ; i++) {
-  Math.floor(number/10) === 0? xx = '0${number}' : xx = String(number);
-  author.avatar = `img/avatars/user' + xx  + '.png`;
-  authorArray[i] = author.avatar;
-  number++;
-}
+  'https://assets.htmlacademy.ru/content/intensive/javascript-1/keksobooking/claire-rendall-b6kAwr1i0Iw.jpg',
+];
 
 const location = {
-  lat: getRandomFloat(35.65000,35.70000),
-  lng: getRandomFloat(139.70000,139.80000)
+  lat: getRandomFloat(35.65000, 35.70000),
+  lng: getRandomFloat(139.70000, 139.80000),
+};
+
+const  getRandomArrayElement = (elements) =>
+  elements [getRandom(0,elements.length - 1)];
+
+function newAuthor(number) {
+  if (number < 10) {
+    return {avatar: 'img/avatars/user0{number}.png'};
+  }
+  return {avatar: 'img/avatars/user0{number}.png'};
 }
 
-const getRandomArrayElement = (elements) => {
-  return elements [positiveInteger(0,elements.length - 1)]
+function getNewArray (arrays) {
+  const newArray = [];
+  for (let i=0; i <= getRandom(0,arrays.length-1); i++) {
+    newArray[i] = arrays[i];
+  }
+  return newArray;
 }
 
-const offer = {
-return {
-  title: 'Объявление',
-  address: '$location.lat,$location.lng'
- price: getRandom(20000,1500000),
- type: getRandomArrayElement(type),
- rooms: getRandom(1,5),
- guests: getRandom(2,10),
-checkin: getRandomArrayElement(checkin),
-checkout: getRandomArrayElement(checkin),
-features:  function () {
-  return Array.from({length:positiveInteger(0,FEATURES.length - 1) },FEATURES)
-},
-description: 'Аппартаменты',
-photos: function () {
-  return Array.from({length:positiveInteger(0,PHOTOS.length - 1) },PHOTOS)
-},
-}
+function createOffer(number) {
+  return {
+    author: newAuthor(number),
+    title: 'Объявление',
+    address: `${location.lat},${location.lng}`,
+    price: getRandom(20000,1500000),
+    type: getRandomArrayElement(TYPE),
+    rooms: getRandom(1,5),
+    guests: getRandom(2,10),
+    checkin: getRandomArrayElement(CHECKIN),
+    checkout: getRandomArrayElement(CHECKIN),
+    features: getNewArray(FEATURES),
+    description: 'Аппартаменты',
+    photos: getNewArray(PHOTOS),
+  };
 }
 
+
+const offers = [];
+for (let i=1; i<=anotherOffers; i++) {
+  offers.push (createOffer(i));
+}
